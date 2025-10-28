5973d49e838d8ad518dccc373a2684ee85d5bc38
sant raj <santraj611@gmail.com>
2025-10-28 21:37:56 +0530

    nothing special

--- src/commands.zig
+++ src/commands.zig
@@ -5,6 +5,7 @@ const cli = @import("cli.zig");
 pub fn getTask(r: *std.Io.Reader, w: *std.Io.Writer) !usize {
     try w.print("What would you like to Add in your TODO list?\n", .{});
     try w.flush();
+    @memset(w.buffer, 0);
     const b_read: usize = try r.streamDelimiter(w, '\n');
     return b_read;
 }
