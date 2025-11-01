c1d4d610c8fb989a22d7cf19acc2b672344e0177
sant raj <santraj611@gmail.com>
2025-11-01 22:34:00 +0530

    now can add task to db

--- src/todo.zig
+++ src/todo.zig
@@ -1,7 +1,21 @@
+const std = @import("std");
+const Db = @import("database.zig");
+
 pub const Task = struct {
     id: usize,
     summery: []const u8,
-    // due_date: ?[]const u8,
-    description: ?[]const u8,
-    completed: bool,
+    description: []const u8,
+    completed: u1, // 1 for completed and 0 for unfinished
 };
+
+/// Adds a task to the database
+pub fn add(alloc: std.mem.Allocator, db: *Db, task: *Task) !void {
+    const query: [:0]const u8 = try std.fmt.allocPrintSentinel(alloc,
+        \\INSERT INTO todos (summery, description, completed) VALUES('{s}', '{s}', {d})
+    , .{ task.summery, task.description, task.completed }, 0);
+    defer alloc.free(query);
+
+    // std.debug.print("summery: {s}\ndescription: {s}\ncompleted: {d}\n", .{ task.summery, task.description, task.completed });
+
+    try db.exec(query);
+}
